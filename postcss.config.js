0963be959870604f6939b6087e08060f2cf042bd
XUXUNKAI <xunkaiflying@qq.com>
2020-09-15 21:56:41 +0800

    首次提交

---
+++ postcss.config.js
@@ -0,0 +1,30 @@
+// module.exports = {
+//     plugins: {
+//       'autoprefixer': {
+//         browsers: ['Android >= 4.0', 'iOS >= 8']
+//       },
+//       'postcss-pxtorem': {
+//         rootValue: 37.5,
+//         propList: ['*']
+//       }
+//     }
+//   }
+
+module.exports = {
+  plugins: {
+    'autoprefixer': {
+      overrideBrowserslist: [
+        "Android 4.1",
+        "iOS 7.1",
+        "Chrome > 31",
+        "ff > 31",
+        "ie >= 8"
+      ]
+    },
+    'postcss-pxtorem': {
+      rootValue: 37.5,
+      propList: ['*']
+    }
+  }
+}
+ 
---
+++ src/store/like/store.js
@@ -0,0 +1,9 @@
+import {state} from './state'
+
+import {mutations} from './mutation'
+
+export const likeModule = {
+  namespaced: true,
+  state,
+  mutations
+}
